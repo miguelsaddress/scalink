0d63378379ed40ca0de68c042caa6053d25a0df7
Miguel Angel Moreno <miguel.s.address@gmail.com>
2017-10-11 00:28:26 +0200

    Add EitherValues to UserRepository tests

--- test/unit/dal/UserRepositorySpec.scala
+++ test/unit/dal/UserRepositorySpec.scala
@@ -1,5 +1,6 @@
 import play.api.Application
 import play.api.test.WithApplication
+import org.scalatest.EitherValues
 
 import business.UserManagement
 import business.adt.User.SignUpData
@@ -11,7 +12,7 @@ import dal.UserRepository.Failures._
 /**
  * @see example https://github.com/knoldus/activator-play-slick-app/blob/master/src/main/g8/test/repo/EmployeeRepositorySpec.scala
  */
-class UserRepositorySpec() extends InvolvesDBSpecification {
+class UserRepositorySpec() extends InvolvesDBSpecification with EitherValues {
 
 
   "Adding a new User" should {
@@ -21,51 +22,26 @@ class UserRepositorySpec() extends InvolvesDBSpecification {
     val userEmailTaken = User(name = "Miguel", username = "other", email = "miguel@example.com", password = "A password")
 
     "Return newly created user after successfull creation" in new WithApplication() {
-
       // it should have been inserted
-      await(userRepository.add(userOk)) match {
-        case Right(user) => {
-          user.name === userOk.name
-          user.username === userOk.username
-          user.email === userOk.email
-          user.password === userOk.password
-        }
-        case Left(failure) => {
-          //forcing test to fail if we reach here
-          true === false
-        }
-      }
-
+      val user = await(userRepository.add(userOk)).right.value
+      user.name === userOk.name
+      user.username === userOk.username
+      user.email === userOk.email
+      user.password === userOk.password
     }
 
     "Return a UsernameTaken when creating a user with an existing username" in new WithApplication() {
-      // it should have been failed due to duplicated usernam
-      await(userRepository.add(userUsernameTaken)) match {
-        case Right(user) => {
-          //forcing test to fail if we reach here
-          true === false
-        }
-        case Left(failure) => {
-          //forcing test to fail if we reach here
-          failure shouldEqual UsernameTaken
-        }
-      }
+      // it should have been failed due to duplicated username
+      val either = await(userRepository.add(userUsernameTaken))
+      either.left.value shouldEqual UsernameTaken
     }
 
     "Return a EmailTaken when creating a user with an existing email" in new WithApplication() {
-      // it should have been failed due to duplicated email. I replace the usernam
-      await(userRepository.add(userEmailTaken)) match {
-        case Right(user) => {
-          //forcing test to fail if we reach here
-          true === false
-        }
-        case Left(failure) => {
-          //forcing test to fail if we reach here
-          failure shouldEqual EmailTaken
-        }
-      }
+      // it should have been failed due to duplicated email. I replace the username
+      val either = await(userRepository.add(userEmailTaken))
+      either.left.value shouldEqual EmailTaken
     }
-  } 
+  }
 
   "Finding a user by Email" should {
     "Return the Option[User] if the Email exists" in new WithApplication() {
